7fc198e12315e2eb7b5e61d204923dfd95cdace0
boudy <khemboudy@gmail.com>
2022-02-16 23:43:30 +0100

    Introduction à la sécurité dans notre application

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * @ORM\Entity(repositoryClass=App\Repository\ProductRepository::class)
  */
 class Product
 {
